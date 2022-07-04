74458229737026e28c85e43aaed2be53464faa6b
Antonio Jose <ajherreratabaco@gmail.com>
2022-07-04 12:01:39 +0200

    login view and bottom logout

--- src/components/layout/Sidebar/Sidebar.js
+++ src/components/layout/Sidebar/Sidebar.js
@@ -3,21 +3,36 @@ import '../../../App.css';
 
 function Sidebar() {
   return (
-    <div className="flex flex-col bg-indigo-700">
-      <p className="font-bold text-2xl">App</p>
-      <NavLink className="" to="/">
+    <div className="container min-h-screen flex gap-y-2 flex-col bg-indigo-600">
+      <p className="font-bold text-2xl text-white text-center">App</p>
+      <NavLink className="flex gap-2 text-white font-light" to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+        </svg>
         Home
       </NavLink>
-      <NavLink className="" to="/students">
+      <NavLink className="flex gap-2 text-white font-light" to="/students">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
         Alumnos
       </NavLink>
-      <NavLink className="" to="/groups">
+      <NavLink className="flex gap-2 text-white font-light" to="/groups">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>
         Grupos
       </NavLink>
-      <NavLink className="" to="/subjects">
+      <NavLink className="flex gap-2 text-white font-light" to="/subjects">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+        </svg>
         Asignaturas
       </NavLink>
-      <NavLink className="" to="/">
+      <NavLink className="flex gap-2 text-white font-light bottom-0 absolute" to="/login">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+        </svg>
         Logout
       </NavLink>
     </div>
